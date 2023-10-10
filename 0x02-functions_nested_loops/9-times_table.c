e1e9279324dd63dfdf98fdebf673e45e0d695491
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-10-10 13:37:38 +0100

    [0x02 c] tsk9 added

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_number - print int numbers
+ * @n: n must be less than 100
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 10)
+	{
+		_putchar(' ');
+		_putchar('0' + n);
+	}
+	else
+	{
+		_putchar('0' + n / 10);
+		_putchar('0' + n % 10);
+	}
+}
+
+/**
+ * times_table - prints the 9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			print_number(i * j);
+
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
