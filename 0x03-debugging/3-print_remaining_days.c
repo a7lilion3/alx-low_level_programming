d27e715393654e4afcf7fd7aebca6f4a238f25af
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-10-11 15:22:15 +0100

    [0x03 - c - task 3] code change, v33

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,7 +22,7 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 
 		if (day == 61)
-			printf("Remaining days: %d\n", 365 - day);
+			printf("Remaining days: %d\n", 366 - day);
 		else
 			printf("Remaining days: %d\n", 366 - day);
 	}
