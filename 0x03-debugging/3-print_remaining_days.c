74f2c69bc22620fc3c6ab6a72e5db694734d835e
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-10-11 14:26:24 +0100

    [0x03 - c - task 3] day 60 fix, v13

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -20,11 +20,15 @@ void print_remaining_days(int month, int day, int year)
 		}
 
 		if (day == 61)
+		{
 			printf("Day of the year: %d\n", day - 1);
 			printf("Remaining days: %d\n", 366 - day);
+		}
 		else
+		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
+		}
 	}
 	else
 	{
