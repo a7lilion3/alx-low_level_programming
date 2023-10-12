17a1bd080f916a8f93e997d7b1caad593d3e6099
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-10-12 10:40:37 +0100

    [0x04 - c - task 0] mandatory files added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase chars
+ * @c: int
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+<h1>0x04-more_functions_nested_loops</h1>
+
+00. isupper<br>
