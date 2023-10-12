8aba164212fa31f1ede27ac9d6cccf1a970c2427
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-10-12 12:43:28 +0100

    [0x04 - c - task 5] hot fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,24 @@
 #include "main.h"
 
+void solution(void);
+
 /**
  * more_numbers - prints from 0 to 14.
  * Return: void
  */
 void more_numbers(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+		solution();
+}
+
+/**
+ * solution - solution of the task
+ * Return: void
+ */
+void solution(void)
 {
 	char i, j;
 
