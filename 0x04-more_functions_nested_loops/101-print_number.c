2bc3f71085dc7d0e2a9135dd14cfbdab312c1948
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-10-13 01:47:17 +0100

    [0x04 - c - task 12] hot fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,41 +2,77 @@
 #include <stdio.h>
 
 /**
+ * check_null - check if it's zero
+ * @n: int
+ * Return: @n
+ */
+int check_null(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+
+	return (n);
+}
+
+/**
+ * check_negative - check if the number negative
+ * @n: int
+ * Return: @n
+ */
+int check_negative(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+
+	return (n);
+}
+
+/**
  * print_number - prints @n
  * @n: int
  * Return: void
  */
 void print_number(int n)
 {
-	int tn;
+	int tn, i;
 
-	if (n == 0)
-	{
-		_putchar('0');
+	if (!check_null(n))
 		return;
-	}
 
-	if (n < 0)
-	{
+	if (check_negative(n) < 0)
 		n = -n;
-		_putchar('-');
+
+	tn = n;
+	i = 1;
+	while (tn >= 10)
+	{
+		i *= 10;
+		tn /= 10;
 	}
 
-	tn = 0;
-	while (1)
+	tn = n;
+	while (tn != 0)
 	{
-		tn += (n % 10);
-		n /= 10;
-		if (n != 0)
-			tn *= 10;
+		if (i != 0)
+			_putchar('0' + tn / i);
 		else
 			break;
-	}
-
-	n = tn;
-	while (n != 0)
-	{
-		_putchar('0' + n % 10);
-		n /= 10;
+		tn %= i;
+		if (tn == 0 && i != 0)
+		{
+			i /= 10;
+			while (i != 0)
+			{
+				i /= 10;
+				_putchar('0');
+			}
+		}
+		else
+			if (i != 0)
+				i /= 10;
 	}
 }
