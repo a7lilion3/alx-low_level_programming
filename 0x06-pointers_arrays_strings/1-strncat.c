61d359df827405b719a37eb7fb71de24d353e33f
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-10-18 23:05:11 +0100

    [0x06 - c - task 1] hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,10 +10,8 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
-	int is_limited;
 	char *t_dest;
 
-	is_limited = 0;
 	t_dest = dest;
 
 	while (*t_dest)
@@ -21,15 +19,9 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; (*t_dest = *src) && (i < n); i++)
 	{
-		if (i >= n - 1)
-			is_limited = 1;
-
 		t_dest++;
 		src++;
 	}
-
-	if (is_limited)
-		*t_dest = '\0';
 
 	return (dest);
 }
