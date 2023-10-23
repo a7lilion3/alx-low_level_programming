7c85eaae5cbdfd862025194a9201262a5060f4d4
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-10-23 19:40:54 +0100

    [0x07 - c - task 2] hot fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - it locates a character in a string
@@ -13,5 +14,5 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] != '\0'; ++i)
 		if (s[i] == c)
 			return (s + i);
-	return (0);
+	return (NULL);
 }
