982b72e86d28574d2ef36485759235155778d138
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-10-30 12:01:02 +0000

    [0x0B - c - task 3] hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,6 +12,9 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **ar;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
 	ar = malloc(height * sizeof(int *));
 	if (ar == NULL)
 		return (NULL);
