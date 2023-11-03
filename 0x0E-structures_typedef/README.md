4a34e67f913bd786a4efd659de69ad4ee419924a
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-11-03 13:02:51 +0000

    [0x0D - c - task 0] mandatory file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+<h1>0x0E-structures_typedef</h1>
+
+00. Poppy<br>
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER
+#define HEADER
+
+/**
+ * struct dog - struct
+ * @name: char *
+ * @age: float
+ * @owner: char *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
