b66e5509caad442d417569159a7b06d84ff427a4
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-11-04 09:08:14 +0000

    [0x0E - c - task 2] mandatory file added

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,19 +11,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	printf("Name: ");
-	if (d->name == NULL || d->name[0] < 10)
-		printf("(nil)\n");
-	else
-		printf("%s\n", d->name);
-
-	if ((char) d->age <= 0)
-		printf("(nil)\n");
-	else
-		printf("Age: %f\n", d->age);
-
-	if (d->owner == NULL || d->owner[0] < 10)
-		printf("(nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+	printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+	printf("Age: %f\n", (d->age) ? d->age : "(nil)");
+	printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 }
