8e080936133d3c4208909efffa73dd4c336838ae
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-11-04 08:16:29 +0000

    [0x0E - c - task 2] mandatory file added

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,7 +17,10 @@ void print_dog(struct dog *d)
 	else
 		printf("%s\n", d->name);
 
-	printf("Age: %f\n", d->age);
+	if (d->age == 0)
+		printf("(nil)\n");
+	else
+		printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("(nil)\n");
