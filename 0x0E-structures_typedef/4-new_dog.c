83c3b00edf156c03916bdd6bc58c731003482381
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-11-04 09:04:12 +0000

    [0x0E - c - task 4] mandatory file added

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,43 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: char *
+ * @age: float
+ * @owner: char *
+ * Return: dog_t *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ndog;
+	int i;
+
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
+
+	if (name == NULL)
+		ndog->name = NULL;
+	else
+	{
+		ndog->name = malloc(64);
+		if (ndog->name != NULL)
+			for (i = 0; (ndog->name[i] = name[i]) != '\0'; ++i)
+				;
+	}
+
+	ndog->age = age;
+
+	if (owner == NULL)
+		ndog->owner = NULL;
+	else
+	{
+		ndog->owner = malloc(64);
+		if (ndog->owner != NULL)
+			for (i = 0; (ndog->owner[i] = owner[i]) != '\0'; ++i)
+				;
+	}
+
+	return (ndog);
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -4,3 +4,4 @@
 01. A dog is the only thing on earth that loves you more than you love yourself<br>
 02. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad<br>
 03. Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read<br>
+04. A door is what a dog is perpetually on the wrong side of<br>
