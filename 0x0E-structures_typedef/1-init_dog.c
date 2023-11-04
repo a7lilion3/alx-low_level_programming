44ccec69b11c4e0ae89ec7c2decaeafbead7b080
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-11-04 07:53:38 +0000

    [0x0E - c - task 1] mandatory file added

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initilize struct dog
+ * @d: struct dog *
+ * @name: char *
+ * @age: float
+ * @owner: char *
+ *Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,3 +1,4 @@
 <h1>0x0E-structures_typedef</h1>
 
 00. Poppy<br>
+01. A dog is the only thing on earth that loves you more than you love yourself<br>
