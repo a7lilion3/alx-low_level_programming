d92e10c15cb0e3e264e020d831f25f2f8c95e8ba
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-11-04 09:56:49 +0000

    [0x0E - c - task 4] mandatory file added

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,7 +21,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		ndog->name = NULL;
 	else
 	{
-		ndog->name = malloc(n);
+		ndog->name = malloc(64);
 		if (ndog->name != NULL)
 			for (i = 0; (ndog->name[i] = name[i]) != '\0'; ++i)
 				;
@@ -33,7 +33,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		ndog->owner = NULL;
 	else
 	{
-		ndog->owner = malloc(n);
+		ndog->owner = malloc(64);
 		if (ndog->owner != NULL)
 			for (i = 0; (ndog->owner[i] = owner[i]) != '\0'; ++i)
 				;
