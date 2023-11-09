a22b0529c044cb79e945ff6fbea9da3cd837b841
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-11-09 14:32:32 +0000

    0x10 - c - task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: const char * const
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	int i, c, iarg, carg;
+	double farg;
+	char *sarg;
+	va_list ag;
+
+	va_start(ag, format);
+	i = 0;
+	while (format[i])
+	{
+		c = format[i];
+		switch (c)
+		{
+			case 'c':
+				carg = va_arg(ag, int);
+				printf("%c", carg);
+				break;
+			case 'i':
+				iarg = va_arg(ag, int);
+				printf("%d", iarg);
+				break;
+			case 'f':
+				farg = va_arg(ag, double);
+				printf("%f", farg);
+				break;
+			case 's':
+				sarg = va_arg(ag, char *);
+				if (sarg == NULL)
+					sarg = "(nil)";
+				printf("%s", sarg);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		i++;
+		if (format[i] != '\0')
+			printf(", ");
+	}
+	va_end(ag);
+	printf("\n");
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -3,3 +3,4 @@
 00. Beauty is variable, ugliness is constant<br>
 01. To be is to be the value of a variable<br>
 02. One woman's constant is another woman's variable<br>
+03. To be is a to be the value of a variable<br>
