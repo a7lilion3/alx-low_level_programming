0be2eef59d3de8085cfc4a41316658f1e0db18b9
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-11-09 13:35:11 +0000

    0x10 - c - task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,10 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; ++i)
 	{
 		printf("%d", va_arg(ag, int));
-		if (i == n - 1)
-			printf("\n");
-		else
+		if (i != n - 1)
 			printf("%s", separator);
 	}
 	va_end(ag);
+
+	printf("\n");
 }
