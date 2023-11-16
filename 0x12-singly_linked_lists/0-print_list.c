35767f1084ba871f3a24dfa179f04403f54122ec
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-11-16 13:10:52 +0100

    0x12 - c - task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print all elements of a linked list
+ * @h: const list_t *
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	size_t len;
+	list_t node = *h;
+
+	len = 0;
+	while (1)
+	{
+		if (node.str)
+			printf("[%u] %s\n", node.len, node.str);
+		else
+			printf("[0] (nil)\n");
+
+		++len;
+
+		if (node.next)
+			node = *(node.next);
+		else
+			break;
+	}
+
+	return (len);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+<h1>0x12-singly_linked_lists</h1>
+
+00. Print list
