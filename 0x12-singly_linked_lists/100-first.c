14c9674f76d3e2736bbe1e7dd865c7161eaad50e
Ahmed Hlilioune <a7lilion3@gmail.com>
2023-11-16 23:49:52 +0100

    0x12 - c - adv task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
  *
  * Return: void
  */
-void _start(void)
+void __attribute__ ((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
